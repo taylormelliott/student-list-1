6e637c6a708f715e196a0358b59f283bbc4cd085
Taylor Elliott <tmikale@gmail.com>
2021-06-15 12:28:20 -0600

    updated access token

--- server.js
+++ server.js
@@ -1,51 +1,54 @@
-const express = require('express')
-const path = require('path')
-const Rollbar = require('rollbar')
+const express = require("express");
+const path = require("path");
+const Rollbar = require("rollbar");
 let rollbar = new Rollbar({
-    accessToken: 'f58476424a6642dfb72af56571521d8c',
-    captureUncaught: true,
-    captureUnhandledRejections: true
-})
+  accessToken: "6858d49b809c45a39ffbcca3cc7428cb",
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use('/style', express.static('./public/styles.css'))
+app.use(express.json());
+app.use("/style", express.static("./public/styles.css"));
 
-let students = []
+let students = [];
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-    rollbar.info('html file served successfully')
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+  rollbar.info("html file served successfully");
+});
 
-app.post('/api/student', (req, res) => {
-    let {name} = req.body
-    name = name.trim()
+app.post("/api/student", (req, res) => {
+  let { name } = req.body;
+  name = name.trim();
 
-    const index = students.findIndex((studentName) => {
-        studentName === name
-    })
+  const index = students.findIndex((studentName) => {
+    studentName === name;
+  });
 
-    try {
-        if (index === -1 && name !== '') {
-            students.push(name)
-            rollbar.log('student added successfully', {author: 'riley', type: 'manual'})
-            res.status(200).send(students)
-        } else if (name === '') {
-            rollbar.error('no name given')
-            res.status(400).send('must provide a name')
-        } else {
-            rollbar.error('student already exists')
-            res.status(400).send('that student already exists')
-        }
-    } catch (err) {
-        rollbar.error(err)
+  try {
+    if (index === -1 && name !== "") {
+      students.push(name);
+      rollbar.log("student added successfully", {
+        author: "riley",
+        type: "manual",
+      });
+      res.status(200).send(students);
+    } else if (name === "") {
+      rollbar.error("no name given");
+      res.status(400).send("must provide a name");
+    } else {
+      rollbar.error("student already exists");
+      res.status(400).send("that student already exists");
     }
-})
+  } catch (err) {
+    rollbar.error(err);
+  }
+});
 
-const port = process.env.PORT || 4545
+const port = process.env.PORT || 4545;
 
-app.use(rollbar.errorHandler())
+app.use(rollbar.errorHandler());
 
-app.listen(port, () => console.log(`running on port: ${port}`))
+app.listen(port, () => console.log(`running on port: ${port}`));
